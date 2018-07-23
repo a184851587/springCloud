4776fd58b4e1fedd8fe045da71cb3c4033d0e6e0
zhangdi <184851587@qq.com>
2018-07-23 15:49:23 +0800

    rest

--- service-hi/src/main/java/com/dufe/servicehi/resource/HiReource.java
+++ service-hi/src/main/java/com/dufe/servicehi/resource/HiReource.java
@@ -2,15 +2,19 @@ package com.dufe.servicehi.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.HashMap;
 import java.util.Map;
 
 @Path("/hi")
 public class HiReource {
     @GET
-    public Map  hi(){
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response hi(){
         Map map = new HashMap();
            map.put("hi","this is jesery test!");
-        return map;
+        return Response.status(Response.Status.OK).entity(map).build();
     }
 }
