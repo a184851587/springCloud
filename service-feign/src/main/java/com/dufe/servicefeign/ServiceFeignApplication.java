7118fae443b99955d8b636f87e6b515cfc2bda0e
zhangdi <184851587@qq.com>
2018-07-27 17:36:09 +0800

    fegin hystrix

--- service-feign/src/main/java/com/dufe/servicefeign/ServiceFeignApplication.java
+++ service-feign/src/main/java/com/dufe/servicefeign/ServiceFeignApplication.java
@@ -7,12 +7,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableEurekaClient
 public class ServiceFeignApplication {
 
     public static void main(String[] args) {
